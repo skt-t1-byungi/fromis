7709a4900a6a6a54fe8519ffd1fb6047807b82b3
byungi <zeronin2@gmail.com>
2020-12-13 01:39:39 +0900

    fix: improve

--- src/index.js
+++ src/index.js
@@ -4,34 +4,36 @@ export default function promis (exec) {
     var _queue = []
     exec(function (v) {
         if (v && typeof v.then === 'function') {
-            v.then(function (v) { done(1, v) })
+            v.then(function (v) { done(1, v) }, reject)
         } else {
             done(1, v)
         }
-    }, function (err) {
+    }, reject)
+    function reject (err) {
         done(2, err)
-    })
+    }
     return {
         then: then,
         'catch': catch_
     }
     function done (state, val) {
-        if (_state > 0) return
+        if (_state) return
         _state = state
         _val = val
         var i = state - 1
         _queue.forEach(function (arr) { arr[i]() })
         _queue = null
     }
-    function then (fn) {
+    function then (thener, catcher) {
         return promis(function (res, rej) {
-            push(createHandler(fn, res, rej), rej)
+            push(
+                thener ? createHandler(thener, res, rej) : res,
+                catcher ? createHandler(catcher, res, rej) : rej
+            )
         })
     }
     function catch_ (fn) {
-        return promis(function (res, rej) {
-            push(res, createHandler(fn, res, rej))
-        })
+        return then(fn)
     }
     function push (thener, catcher) {
         var tick = setTimeout
