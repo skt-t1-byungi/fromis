c00e4b98eebd9d07063fd5f799fb6a4ec611b462
byungi <zeronin2@gmail.com>
2020-12-13 21:19:57 +0900

    test

--- src/test.js
+++ src/test.js
@@ -33,21 +33,23 @@ test('reject', async () => {
 test('chaining', async () => {
     promis(r => r(1))
         .then(push)
+        .then(push)
         .then(v => v + 2)
         .then(push)
     $.equal(buf, [])
     await nextTick()
     $.equal(buf, [1])
-    await nextTick(2)
-    $.equal(buf, [1, 3])
+    await nextTick(3)
+    $.equal(buf, [1, 1, 3])
 })
 
 test('catch', async () => {
     promis((_, rej) => rej(1))
         .then(push)
+        .then(push)
         .catch(v => v + 2)
         .then(push)
-    await nextTick(3)
+    await nextTick(4)
     $.equal(buf, [3])
 })
 
