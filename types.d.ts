eefb905ecba56ebd4234c4158a7d6942c0550f57
byungi <zeronin2@gmail.com>
2020-12-18 23:16:54 +0900

    chore

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 interface Thenable<T> {
-    then<V1,V2=never>(thener?: (val:T)=>V1|Thenable<V1>, catcher?: (err:any)=>V2|Thenable<V2>):Thenable<V1|V2>,
-    catch<V>(catcher: (err:any)=>V|Thenable<V>):Thenable<V>,
+    then<V1, V2=never>(thener?: (val: T) => V1|Thenable<V1>, catcher?: (err: any) => V2|Thenable<V2>): Thenable<V1|V2>;
+    catch<V>(catcher: (err: any) => V|Thenable<V>): Thenable<V>;
 }
 
-declare function promise<V>(exec: (resolve:(v:V|Thenable<V>)=>void, reject: (err?:any)=>void)=>void):Thenable<V>
+declare function fromis<V>(exec: (resolve: (v: V|Thenable<V>) => void, reject: (err?: any) => void) => void): Thenable<V>
 
-export default promise
+export default fromis
