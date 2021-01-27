71dee5894f0fcabca387b540b5b7ce8fcd652a8c
byungi <tiniwz@gmail.com>
2021-01-27 10:36:16 +0900

    chore: eslint -> prettier

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
-export default function fromis (exec) {
+export default function fromis(exec) {
     return _fromis(false, exec)
 }
-function _fromis (sync, exec) {
+
+function _fromis(sync, exec) {
     var _state = -1 // -1:pending, 0:resolved, 1:rejected
     var _val = null
     var _queue = []
     exec(function (v) {
-        if (v && typeof v.then === 'function') {
-            v.then(function (v) { done(0, v) }, reject)
+        if (v && typeof v.then === "function") {
+            v.then(function (v) {
+                done(0, v)
+            }, reject)
         } else {
             done(0, v)
         }
     }, reject)
-    function reject (err) {
+    function reject(err) {
         done(1, err)
     }
     return {
         then: then,
-        'catch': function (fn) { return then(null, fn) }
+        catch: function (fn) {
+            return then(null, fn)
+        },
     }
-    function done (state, val) {
+    function done(state, val) {
         if (~_state) return
         _state = state
         _val = val
@@ -29,22 +34,24 @@ function _fromis (sync, exec) {
             setTimeout(flush)
         }
     }
-    function flush () {
-        _queue.forEach(function (arr) { arr[_state](_val) })
+    function flush() {
+        _queue.forEach(function (arr) {
+            arr[_state](_val)
+        })
         _queue = null
     }
-    function then (thener, catcher) {
+    function then(thener, catcher) {
         return _fromis(true, function (res, rej) {
             var arr = [
                 thener ? wrap(thener) : res,
-                catcher ? wrap(catcher) : rej
+                catcher ? wrap(catcher) : rej,
             ]
             if (_queue) {
                 _queue.push(arr)
             } else {
                 arr[_state](_val)
             }
-            function wrap (fn) {
+            function wrap(fn) {
                 return function (v) {
                     try {
                         v = fn(v)
--- src/test.js
+++ src/test.js
@@ -4,20 +4,23 @@ import * as $ from 'uvu/assert'
 import fromis from '.'
 
 const buf = []
-const push = v => (buf.push(v), v)
-const clear = () => { buf.length = 0 }
-const nextTick = (n = 1) => new Promise(function recur (res) {
-    if (n--) {
-        setTimeout(recur, 0, res)
-    } else {
-        res()
-    }
-})
+const push = (v) => (buf.push(v), v)
+const clear = () => {
+    buf.length = 0
+}
+const nextTick = (n = 1) =>
+    new Promise(function recur(res) {
+        if (n--) {
+            setTimeout(recur, 0, res)
+        } else {
+            res()
+        }
+    })
 
 test.before.each(clear)
 
 test('resolve', async () => {
-    $.is(await fromis(res => res('resolved')), 'resolved')
+    $.is(await fromis((res) => res('resolved')), 'resolved')
 })
 
 test('reject', async () => {
@@ -31,10 +34,10 @@ test('reject', async () => {
 })
 
 test('chaining', async () => {
-    fromis(r => r(1))
+    fromis((r) => r(1))
         .then(push)
         .then(push)
-        .then(v => v + 2)
+        .then((v) => v + 2)
         .then(push)
     $.equal(buf, [])
     await nextTick()
@@ -45,36 +48,35 @@ test('catch', async () => {
     fromis((_, rej) => rej(1))
         .then(push)
         .then(push)
-        .catch(v => v + 2)
+        .catch((v) => v + 2)
         .then(push)
     await nextTick()
     $.equal(buf, [3])
 })
 
 test('flattening', async () => {
-    fromis(r1 => r1(fromis(r2 => r2(1))))
-        .then(push)
+    fromis((r1) => r1(fromis((r2) => r2(1)))).then(push)
     await nextTick(2)
     $.equal(buf, [1])
     clear()
 
-    fromis(r => r())
-        .then(() => fromis(r => r(2)))
+    fromis((r) => r())
+        .then(() => fromis((r) => r(2)))
         .then(push)
     await nextTick(2)
     $.equal(buf, [2])
     clear()
 
-    fromis(r => r(fromis((_, rej) => rej(3))))
+    fromis((r) => r(fromis((_, rej) => rej(3))))
         .then(push)
-        .catch(v => v + 2)
+        .catch((v) => v + 2)
         .then(push)
     await nextTick(2)
     $.equal(buf, [5])
     clear()
 
     fromis((_, rej) => rej())
-        .catch(() => fromis(r => r(4)))
+        .catch(() => fromis((r) => r(4)))
         .then(push)
     await nextTick(2)
     $.equal(buf, [4])
